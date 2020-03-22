fb3ed480a85f33f4ce4a61d7e844873738b429d0
Doug <me@douglasbowen.com>
2020-03-22 12:50:20 -0400

    add geocoder gmaps test

--- wfgc-frontend/src/components/googMaps.js
+++ wfgc-frontend/src/components/googMaps.js
@@ -1,15 +1,43 @@
 import React from "react";
 import { Browserrouter as Router, Route, Link } from "react-router-dom";
-import { GoogleApiWrapper } from "google-maps-react";
+import { GoogleApiWrapper, Map, InfoWindow, Marker } from "google-maps-react";
 
 export class GoogMaps extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {};
   }
+
+  geoTester = () => {
+    const geocoder = new this.props.google.maps.Geocoder();
+    let cb = (testArray, bigStatus) => {
+      console.log(testArray[0].geometry.bounds.Ta.g);
+      console.log(testArray[0].geometry.bounds.Ya.g);
+      console.log(bigStatus);
+
+      /*
+      return (
+        this.props.google.maps.GeocoderResult,
+        this.props.google.maps.GeocoderStatus
+      );
+*/
+    };
+    geocoder.geocode({ address: "16803" }, cb);
+  };
+
   render() {
     return (
       <div className="gMapsTest">
-        <p>Hello world</p>
+        {this.geoTester()}
+        <Map
+          google={this.props.google}
+          initialCenter={{
+            lat: 55.53,
+            lng: 9.4
+          }}
+          zoom={15}
+          onClick={this.onMapClicked}
+        />
       </div>
     );
   }
