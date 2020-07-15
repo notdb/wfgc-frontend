ec30ea343b992f43d664d119384afa2736ad619e
Doug <me@douglasbowen.com>
2020-07-14 22:52:31 -0400

    change App.test.js

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -4,15 +4,3 @@ describe("My First Test", () => {
   });
 });
 
-describe("My First Test", () => {
-  it("Does not do much!", () => {
-    expect(true).to.equal(false);
-  });
-});
-
-describe("My First Test", () => {
-  it("Visit the Kitchen Sink", () => {
-    cy.visit("https://example.cypress.io");
-    cy.contains("type");
-  });
-});
