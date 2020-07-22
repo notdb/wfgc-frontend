4c93eb0b8b7bad34980ec20471190b4c10053334
Doug <me@douglasbowen.com>
2020-07-21 22:32:58 -0400

    update app.spec.js

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,8 +1,11 @@
 //import ListOfArcades from "../../src/components/listOfArcades.js";
 
 describe("Visits homepage", () => {
-  it("test", () => {
+  it("Loads the page", () => {
     cy.visit("http://localhost:4550");
     //cy.get(".arcades-list").contains("List of top Arcades");
   });
+  it("Arcades-list has the list of arcades", ()=>{
+    cy.get(".arcades-list").contains("List of top Arcades");
+  })
 });
