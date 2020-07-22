33c44be7d711fe6f123d5b18c383bc26f78ba0f6
Doug <me@douglasbowen.com>
2020-07-21 22:59:58 -0400

    change for app.spec.js

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,10 +1,9 @@
 //import ListOfArcades from "../../src/components/listOfArcades.js";
 
 describe("Visits homepage", () => {
-  it("Loads the page", () => {
+  beforeEach(() => {
     cy.visit('/');
-    //cy.get(".arcades-list").contains("List of top Arcades");
-  });
+  })
   it("Arcades-list has the list of arcades", ()=>{
     cy.get(".arcades-list").contains("List of top Arcades");
   })
