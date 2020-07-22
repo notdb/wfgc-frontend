bd6d8f813f388c862e31bfd91e02f38ec0c69ae6
Doug <me@douglasbowen.com>
2020-07-21 22:07:46 -0400

    attempted port change for app.spec.js

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -2,7 +2,7 @@
 
 describe("Visits homepage", () => {
   it("test", () => {
-    cy.visit("http://localhost:3000");
+    cy.visit("http://localhost:4550");
     cy.get(".arcades-list").contains("List of top Arcades");
   });
 });
