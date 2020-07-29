02f36baca23d4eb8d6b455764270e6b734694415
Doug <me@douglasbowen.com>
2020-07-28 21:32:30 -0400

    test new timeout for beforeEach hook

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,11 +1,13 @@
 //import ListOfArcades from "../../src/components/listOfArcades.js";
 
 describe("Visits homepage", () => {
-  beforeEach((done)=>{
-    cy.visit('http://localhost:4550/', { timeout: 30000 }, {onload: ()=>{
+  beforeEach(()=> {
+    cy.visit('http://localhost:4550/', {timeout: 30000})
+    /*
+    {onload: ()=>{
       console.log('hi')
-    }})
-    console.log(done);
+    }}
+    */
   })
   describe('Soemthing', ()=>{
   it("Arcades-list has the list of arcades", ()=>{
