e13466f3841ae2784053268a1e86a506ef1941b3
Doug <me@douglasbowen.com>
2020-07-28 21:15:02 -0400

    add parameter to beforeEach function

--- cypress/integration/app.spec.js
+++ cypress/integration/app.spec.js
@@ -1,8 +1,9 @@
 //import ListOfArcades from "../../src/components/listOfArcades.js";
 
 context("Visits homepage", () => {
-  beforeEach(()=>{
+  beforeEach((done)=>{
     cy.visit('http://localhost:4550/');
+    console.log(done);
   })
   describe('Soemthing', ()=>{
   it("Arcades-list has the list of arcades", ()=>{
